9dc2234bf0aa2ac15f2310e138e79626394e65b6
Boss <C:\Users\\AppData\Roaming\The Bat!>
2021-08-20 00:35:48 +0300

    feat: implement a call to the YouTube API from detailed information page

--- youtube-client/src/app/youtube/pages/info-page/info-page.component.ts
+++ youtube-client/src/app/youtube/pages/info-page/info-page.component.ts
@@ -1,30 +1,34 @@
-import { Location } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
+import { Observable } from 'rxjs';
+import { map, pluck } from 'rxjs/operators';
 
 import { YoutubeService } from '@app/youtube/services/youtube/youtube.service';
 import SearchResultsItem from '@youtube/models/search-results-item.model';
+import YoutubeResponse from '@app/youtube/models/youtube-response/youtube-response.model';
+import { getParsedYoutubeResponse } from '@app/youtube/common/tools';
 
 @Component({
   selector: 'app-info-page',
   templateUrl: './info-page.component.html',
   styleUrls: ['./info-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class InfoPageComponent implements OnInit {
-  infoCard?: SearchResultsItem;
+  infoCard$?: Observable<SearchResultsItem>;
 
-  constructor(
-    private _activatedRoute: ActivatedRoute,
-    private _youtubeService: YoutubeService,
-    private _location: Location,
-  ) { }
+  constructor(private _activatedRoute: ActivatedRoute, private _youtubeService: YoutubeService) { }
 
   ngOnInit(): void {
     const { id } = this._activatedRoute.snapshot.params;
-    this.infoCard = this._youtubeService.getSearchResultsItemById(id);
-  }
 
-  goBack(): void {
-    this._location.back();
+    this.infoCard$ = this._youtubeService.fetchSearchResultsWithStats$(id)
+      .pipe(
+        map<YoutubeResponse, SearchResultsItem[]>(
+          (response: YoutubeResponse) => getParsedYoutubeResponse(response),
+        ),
+        pluck(0),
+      );
   }
 }
